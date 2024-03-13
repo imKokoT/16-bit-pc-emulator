2cd3b4116573fe0beeaf3595d7cdc5893927ea72
KokoT <prikolkillercat@gmail.com>
2024-03-13 19:56:16 +0100

    configure project to displaying unicode
    	modified:   .editorconfig
        modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
+#include <locale>
+#include <codecvt>
+#include <io.h>
+#include <fcntl.h>
 
 int main() {
-    std::cout << "Hello, World!\n";
+    std::locale::global(std::locale("en_US.UTF-8"));
+    _setmode(_fileno(stdout), _O_U8TEXT); 
+
+    std::wcout << L"Hello, тест!\n";
     std::system("pause");
     return 0;
-    
 }
