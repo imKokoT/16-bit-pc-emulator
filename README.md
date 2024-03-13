84e898a7148037de13d7f3c20395e766fcdac242
KokoT <prikolkillercat@gmail.com>
2024-03-13 20:23:04 +0100

    new file:   README.md 	new file:   src/instruction-set.h

---
+++ README.md
@@ -0,0 +1,21 @@
+# trying to create own pc
+    For fun... and because every programmer should to create its cpu emulator, compiler for it and basic operating system lol.
+
+# Instructions
+
+    BAS1:
+        1. LTR
+        2. STM
+        3. INC
+        4. DEC
+        5. ADD
+        6. NOT
+        7. XOR
+        8. OR
+        9. AND
+        10. SHR
+        11. SHL
+        12. JMP
+        13. IOG
+        14. IOS
+        15. CMP
---
+++ src/instruction-set.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __INSTRUCTION_SET_H__
+#define __INSTRUCTION_SET_H__
+
+// First instruction set Basic Architecture Set V1
+enum class BAS1{
+    LTR = 0x00, // load to register
+    STM = 0x10, // set to memory
+    INC = 0x20, // increment
+    DEC = 0x30, // decrement
+    ADD = 0x40, 
+    NOT = 0x50,
+    XOR = 0x60,
+    OR = 0x70,
+    AND = 0x80,
+    SHR = 0x90, // shift right
+    SHL = 0xA0, // shift left
+    JMP = 0xB0, // jump
+    IOG = 0xC0, // io get
+    IOS = 0xD0, // io set
+    CMP = 0xE0, // compare
+};
+
+
+#endif // !__INSTRUCTION_SET_H__
