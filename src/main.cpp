48a2113750a2db4a0f691c5c5bf7d0a63e7e962d
KokoT <prikolkillercat@gmail.com>
2025-05-11 21:07:05 +0200

    oh. windows is s**t but i fixed it

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,15 @@
 #include <io.h>
 #include <fcntl.h>
 
-int main() {
-    std::locale::global(std::locale("en_US.UTF-8"));
-    _setmode(_fileno(stdout), _O_U8TEXT); 
 
-    std::wcout << L"Hello, тест!\n";
-    std::system("pause");
+int main() {
+    std::setlocale(LC_ALL, "en_US.UTF-8");
+    std::wcout.imbue(std::locale("en_US.UTF-8"));
+
+    std::wcout << L"Hello, тест españel!\n";
+    wprintf(L"Hello, тест españel!\n");
+
+    std::cout << "Hello, тест españel!\n";
+    printf("Hello, тест españel!\n");
     return 0;
 }
