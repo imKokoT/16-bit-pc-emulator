a831de975343fb00a27f136d62142f2de9693b53
KokoT <prikolkillercat@gmail.com>
2025-05-12 13:43:49 +0200

    added instructions

--- src/instructions.h
+++ src/instructions.h
@@ -2,23 +2,26 @@
 #ifndef __INSTRUCTION_SET_H__
 #define __INSTRUCTION_SET_H__
 
-// First instruction set Basic Architecture Set V1
-enum class Instructions {
-    LTR = 0x00, // load to register
-    STM = 0x10, // set to memory
-    INC = 0x20, // increment
-    DEC = 0x30, // decrement
-    ADD = 0x40, 
-    NOT = 0x50,
-    XOR = 0x60,
-    OR = 0x70,
-    AND = 0x80,
-    SHR = 0x90, // shift right
-    SHL = 0xA0, // shift left
-    JMP = 0xB0, // jump
-    IOG = 0xC0, // io get
-    IOS = 0xD0, // io set
-    CMP = 0xE0, // compare
+
+enum class Instruction {
+    IDL = 0x00,
+    MOV = 0x01,
+
+    // ALU
+    INC,
+    DEC,
+    ADD,
+    SUB,
+    XOR,
+    OR,
+    AND,
+    NOT,
+    SHR,
+    SHL,
+
+    // Branch
+    CMP,
+    JMP,
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,5 @@ int main() {
     std::setlocale(LC_ALL, "en_US.UTF-8");
     std::wcout.imbue(std::locale("en_US.UTF-8"));
 
-    std::wcout << L"Hello, тест españel!\n";
-    wprintf(L"Hello, тест españel!\n");
-
-    std::cout << "Hello, тест españel!\n";
-    printf("Hello, тест españel!\n");
     return 0;
 }
